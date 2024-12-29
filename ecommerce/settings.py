dd3f403064c9e2a3f3ebbaf3f116137ea5ec824b
shafeer2511 <126599497+shafeer2511@users.noreply.github.com>
2024-12-29 21:53:15 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL='/images/'
+MEDIA_ROOT=BASE_DIR/'static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
