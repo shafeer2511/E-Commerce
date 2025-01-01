6f834ef0dd9b6e3682854f8fdf9a52191283b0be
shafeer2511 <126599497+shafeer2511@users.noreply.github.com>
2025-01-01 21:39:43 +0530

    Update admin.py

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=('name','image','description','created_at')
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'vendor', 'quantity', 'original_price', 'selling_price', 'status', 'trending', 'created_at')
+    search_fields = ('name', 'vendor')  # Adds a search bar for these fields
+    list_filter = ('status', 'trending', 'category')  # Adds filters in the admin sidebar
+    ordering = ('-created_at',)  # Orders products by the newest created first
 
-admin.site.register(Category)
-admin.site.register(Product) 
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Product,ProductAdmin)
