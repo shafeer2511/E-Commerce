fc112c8a02fe50c49353b95483118b145757e9cf
shafeer2511 <126599497+shafeer2511@users.noreply.github.com>
2025-01-02 23:01:44 +0530

    Update views.py

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
+from . models import *
 def home(request):
     return render(request,'shop/index.html')
 
 def register(request):
     return render(request,'shop/register.html')
+
+def collection(request):
+    catagory=Category.objects.filter(status=1)
+    return render(request,'shop/collection.html',{'catagory':catagory})
