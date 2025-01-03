133fad5d2e94f795fff69a816e868e520019dab8
shafeer2511 <126599497+shafeer2511@users.noreply.github.com>
2025-01-03 23:12:41 +0530

    Update views.py

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from . models import *
+from django.contrib import messages
 def home(request):
     return render(request,'shop/index.html')
 
@@ -9,3 +10,23 @@ def register(request):
 def collection(request):
     catagory=Category.objects.filter(status=1)
     return render(request,'shop/collection.html',{'catagory':catagory})
+
+def collectionview(request,name):
+    if(Category.objects.filter(name=name,status=1)):
+        products=Product.objects.filter(category__name=name)
+        return render(request,'shop/products/products.html',{'products':products,'category_name':name})
+    else:
+        messages.warning(request,'No such categories found')
+        return redirect('collection')
+    
+def product_details(request,cname,pname):
+    if(Category.objects.filter(name=cname,status=1)):
+        if(Product.objects.filter(name=pname,status=1)):
+            products=Product.objects.filter(name=pname,status=1).first()
+            return render(request,'shop/products/product_details.html',{'products':products})
+        else:
+            messages.warning(request,"No such product found")
+            return redirect('collection')
+    else:
+        messages.warning(request,"No such category found")
+        return redirect('collection')
