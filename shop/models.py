33a12390c4e4f15218afdb05ebbed559cee6d2ff
shafeer2511 <126599497+shafeer2511@users.noreply.github.com>
2025-01-20 21:16:49 +0530

    Update models.py

--- shop/models.py
+++ shop/models.py
@@ -53,3 +53,8 @@ class Cart(models.Model):
     @property
     def total_cost(self):
         return self.product_qty*self.product.selling_price
+    
+class Favorite(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
